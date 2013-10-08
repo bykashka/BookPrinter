b7f910547cbecab527d043e49cb6f61a8fcb3adf
Alexander <zhykovetskyi.alex@gmail.com>
2013-10-08 23:39:32 +0300

    small refactoring

--- wx_gui/wx_book_printer.py
+++ wx_gui/wx_book_printer.py
@@ -22,16 +22,19 @@ class Book_printer(wx.Frame):
         menu2.AppendSeparator()
         about = menu2.Append(wx.NewId(), '&About')
         menuBar.Append(menu2, '&Help')
+
+        self.SetMenuBar(menuBar)
+
         string = '> или < рамка в см.'
         self.border = wx.CheckBox(self.panel, -1, string, (10, 30), (150, 20))
         self.border_vlue = wx.TextCtrl(self.panel, -1, '0.0', (160, 30), (50, 20))
         self.border_vlue.Enable(False)
-        wx.StaticText(self.panel, -1, 'Страниц в книге:', (10, 50), (150, 20))
-        self.book_pages = wx.TextCtrl(self.panel, -1, '0', (160, 50), (50, 20))
-        wx.StaticText(self.panel, -1, 'Страниц в тетраде:', (10, 70), (150, 20))
-        self.part_pages = wx.TextCtrl(self.panel, -1, '32', (160, 70), (50, 20))
+        wx.StaticText(self.panel, -1, 'Страниц в книге:', (10, 55), (150, 20))
+        self.book_pages = wx.TextCtrl(self.panel, -1, '0', (160, 55), (50, 20))
+        wx.StaticText(self.panel, -1, 'Страниц в тетраде:', (10, 80), (150, 20))
+        self.part_pages = wx.TextCtrl(self.panel, -1, '32', (160, 80), (50, 20))
 
-        self.btn_print = wx.Button(self.panel, -1, 'Печать', (10, 90), (200, 20))
+        self.btn_print = wx.Button(self.panel, -1, 'Печать', (10, 105), (200, 25))
 
         self.Bind(wx.EVT_MENU, self.OnOpen, open_file)
         self.Bind(wx.EVT_MENU, self.OnExit, exit)
@@ -41,7 +44,7 @@ class Book_printer(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnPrint, self.btn_print)
 ##        self.Bind(wx.EVT_IDLE, self.SowGauge)
 
-        self.SetMenuBar(menuBar)
+
 
         self.select = wx.StaticText(self.panel, -1, 'Выбор:', (10, 10), (150, 20))
 
@@ -85,22 +88,22 @@ class Book_printer(wx.Frame):
             path_to_book_old, 
             self.path_to_book,])
 
-        # Set work directory
+        # Set working directory
         os.chdir(os.path.split(self.path_to_book)[0])
 
-        # Create new work directory
+        # Create new working directory
         dir_name = os.path.splitext(os.path.basename(self.path_to_book))[0]
         self.work_dir = os.path.join(os.getcwd(), dir_name)
         if not os.path.exists(self.work_dir):
 			os.mkdir(self.work_dir)
 
-        # Set new work directory
+        # Set new working directory
         os.chdir(self.work_dir)
 
-        # Get pages in book
+        # Get count of book pages
         pages = int(self.book_pages.GetValue())
 
-        # Get pages in byklet
+        # Get count of byklet pages
         byklet_pages = int(self.part_pages.GetValue())
 
         # Add some empty pages
@@ -146,7 +149,7 @@ class Book_printer(wx.Frame):
                     in_file,
                     output_file,])
 
-        # 1 list formats A4 -> 2 lists format A5
+        # 1 list format A4 -> 2 lists format A5
 
         for in_byklet_file in all_byklet_files:
             f_name = os.path.basename(in_byklet_file)
@@ -166,7 +169,7 @@ class Book_printer(wx.Frame):
             os.remove(p)
             #print ".",
             
-            #print "\nDeleyting byklet files"
+            #print "\nDeleting byklet files"
         for p in all_byklet_files:
             os.remove(p)
             #print ".",
